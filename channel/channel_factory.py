bac42f44f4b535e3c51185f3d950bc8373a62035
FutureNav <support@zhenxiang.life>
2023-09-04 13:10:45 +0800

    还原试一下

--- channel/channel_factory.py
+++ channel/channel_factory.py
@@ -33,8 +33,4 @@ def create_channel(channel_type):
         from channel.wechatcom.wechatcomapp_channel import WechatComAppChannel
 
         return WechatComAppChannel()
-    elif channel_type == "wework":
-        from channel.wework.wework_channel import WeworkChannel
-
-        return WeworkChannel()
     raise RuntimeError
--- common/const.py
+++ common/const.py
@@ -8,5 +8,4 @@ LINKAI = "linkai"
 
 VERSION = "1.3.0"
 
-CLAUDEAI = "claude"
-MODEL_LIST = ["gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "wenxin", "xunfei","claude"]
+MODEL_LIST = ["gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "wenxin", "xunfei"]
